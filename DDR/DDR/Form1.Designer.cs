be7cd4422c70f7d6a10d8583ff87327572f3d209
CASTEGNARO EROS <10933991@itisrossi.edu.it>
2025-03-01 09:43:59 +0100

    messe immagini

---
+++ DDR/DDR/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace DDR
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.btn_up = new System.Windows.Forms.Button();
+            this.btn_right = new System.Windows.Forms.Button();
+            this.btn_down = new System.Windows.Forms.Button();
+            this.btn_left = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 3;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.Controls.Add(this.btn_up, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.btn_right, 2, 1);
+            this.tableLayoutPanel1.Controls.Add(this.btn_down, 1, 2);
+            this.tableLayoutPanel1.Controls.Add(this.btn_left, 0, 1);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(200, 350);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 3;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(400, 400);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // btn_up
+            // 
+            this.btn_up.Location = new System.Drawing.Point(136, 3);
+            this.btn_up.Name = "btn_up";
+            this.btn_up.Size = new System.Drawing.Size(127, 127);
+            this.btn_up.TabIndex = 0;
+            this.btn_up.Text = "button1";
+            this.btn_up.UseVisualStyleBackColor = true;
+            this.btn_up.Click += new System.EventHandler(this.btn_up_Click);
+            // 
+            // btn_right
+            // 
+            this.btn_right.Location = new System.Drawing.Point(269, 136);
+            this.btn_right.Name = "btn_right";
+            this.btn_right.Size = new System.Drawing.Size(128, 127);
+            this.btn_right.TabIndex = 1;
+            this.btn_right.Text = "button1";
+            this.btn_right.UseVisualStyleBackColor = true;
+            this.btn_right.Click += new System.EventHandler(this.btn_right_Click);
+            // 
+            // btn_down
+            // 
+            this.btn_down.Location = new System.Drawing.Point(136, 269);
+            this.btn_down.Name = "btn_down";
+            this.btn_down.Size = new System.Drawing.Size(127, 128);
+            this.btn_down.TabIndex = 2;
+            this.btn_down.Text = "button1";
+            this.btn_down.UseVisualStyleBackColor = true;
+            this.btn_down.Click += new System.EventHandler(this.btn_down_Click);
+            // 
+            // btn_left
+            // 
+            this.btn_left.Location = new System.Drawing.Point(3, 136);
+            this.btn_left.Name = "btn_left";
+            this.btn_left.Size = new System.Drawing.Size(127, 127);
+            this.btn_left.TabIndex = 3;
+            this.btn_left.Text = "button1";
+            this.btn_left.UseVisualStyleBackColor = true;
+            this.btn_left.Click += new System.EventHandler(this.btn_left_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 761);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Button btn_up;
+        private System.Windows.Forms.Button btn_right;
+        private System.Windows.Forms.Button btn_down;
+        private System.Windows.Forms.Button btn_left;
+    }
+}
+
---
+++ DDR/DDR/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DDR
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_up_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_left_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_down_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_right_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
